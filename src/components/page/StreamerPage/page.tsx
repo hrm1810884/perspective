930fe34b5733d526e009c9a9df01616323ff9869
Haruma HIRABAYASHI <hrm1810884@g.ecc.u-tokyo.ac.jp>
2024-07-04 04:51:23 +0900

    fix reset text

--- src/components/page/StreamerPage/page.tsx
+++ src/components/page/StreamerPage/page.tsx
@@ -1,16 +1,14 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 import { Button, Textarea } from "@mantine/core";
-import { useCallback, useEffect } from "react";
-import { match } from "ts-pattern";
+import { useEffect, useRef } from "react";
 
-import { experienceDataList } from "@/models";
-import { useExperenceStates, useTypingSound } from "@/states";
+import { useExperenceStates, useTyping } from "@/states";
 
 import { demoInput } from "./consts";
 import { useStreamer } from "./hooks";
 
-import { DemoModal, EndModal, StartModal } from "./components";
+import { ExperienceModal } from "./components/ExperienceModal";
 
 import {
     buttonStyle,
@@ -29,37 +27,66 @@ export const StreamerPage = () => {
     } = useStreamer();
 
     const {
-        handler: { handleShortTypingSound, handleTypingText },
-    } = useTypingSound();
+        handler: { handleShortTypingSound },
+    } = useTyping();
 
-    const { experienceState, diaryHandler, demoHandler } = useExperenceStates();
+    const {
+        experienceState,
+        stageSwitcher,
+        mutator: { setStage },
+    } = useExperenceStates();
 
-    const handleEndButtonClick = useCallback(() => {
-        match(experienceState.mode)
-            .with("Diary", () => {
-                diaryHandler.handleFinish();
-            })
-            .with("Demo", () => {
-                demoHandler.handleInit();
-            });
-    }, [experienceState, diaryHandler, demoHandler]);
-
-    const handleBackButtonClick = useCallback(() => {
-        demoHandler.handleSelectDemo();
-    }, [demoHandler]);
+    const timeoutRef = useRef<number | null>(null);
 
     useEffect(() => {
+        const clearExistingTimeout = () => {
+            if (timeoutRef.current) {
+                clearTimeout(timeoutRef.current);
+                timeoutRef.current = null;
+            }
+        };
+
+        const startTextUpdate = (demoText: string) => {
+            let index = 1; // Initialize index to 1 to start with the first character
+
+            const updateWithRandomInterval = () => {
+                if (index <= demoText.length) {
+                    updateText(demoText.slice(0, index));
+                    index++;
+
+                    // Generate a random duration between 100 and 400 ms for the next update
+                    const randomDuration = Math.floor(Math.random() * 300) + 100;
+
+                    // Set the next timeout
+                    timeoutRef.current = window.setTimeout(
+                        updateWithRandomInterval,
+                        randomDuration
+                    );
+                }
+            };
+
+            // Start the first update with an initial random interval
+            updateWithRandomInterval();
+        };
+
         if (experienceState.stage !== "finish") {
-            handleReset(); //finishの時は保存するのでresetしない
+            handleReset(); // Call handleReset if stage is not "finish"
         }
-        if (experienceState.mode === "Demo" && experienceState.selection) {
-            const demoText = demoInput[experienceState.selection.key];
-            handleTypingText(demoText);
+
+        if (experienceState.stage === "demo" && experienceState.demoSelection) {
+            const demoText = demoInput[experienceState.demoSelection.key];
+            startTextUpdate(demoText);
+        } else {
+            clearExistingTimeout();
         }
-    }, [experienceState, updateText]);
+
+        return () => {
+            clearExistingTimeout();
+        };
+    }, [experienceState, updateText, handleReset, demoInput]);
 
     useEffect(() => {
-        if (experienceState.mode === "Demo" && experienceState.selection) {
+        if (experienceState.stage === "demo" && experienceState.demoSelection) {
             handleShortTypingSound();
             handleInputChange(clientText);
         }
@@ -67,9 +94,7 @@ export const StreamerPage = () => {
 
     return (
         <div className={wrapper}>
-            <StartModal />
-            <DemoModal />
-            <EndModal />
+            <ExperienceModal />
             <Textarea
                 classNames={{ root: textAreaRootStyle, input: textAreaInputStyle }}
                 value={clientText}
@@ -79,17 +104,25 @@ export const StreamerPage = () => {
                 }}
                 placeholder="Write message"
                 ref={textareaRef}
-                disabled={experienceState.mode === "Demo"}
+                disabled={experienceState.stage === "demo"}
             />
 
             <div className={controlAreaStyle}>
-                {experienceState.mode !== null && (
-                    <Button onClick={handleEndButtonClick} className={buttonStyle}>
-                        {`${experienceDataList.find((item) => item.mode === experienceState.mode)?.label}を終了する`}
-                    </Button>
-                )}
-                {experienceState.mode === "Demo" && (
-                    <Button onClick={handleBackButtonClick} className={buttonStyle}>
+                {(experienceState.stage === "demo" || experienceState.stage === "diary") &&
+                    stageSwitcher({
+                        demo: (
+                            <Button onClick={() => setStage("diary")} className={buttonStyle}>
+                                {"デモを終了する"}
+                            </Button>
+                        ),
+                        diary: (
+                            <Button onClick={() => setStage("finish")} className={buttonStyle}>
+                                {"体験を終了する"}
+                            </Button>
+                        ),
+                    })}
+                {experienceState.stage === "demo" && (
+                    <Button onClick={() => setStage("demo")} className={buttonStyle}>
                         他のデモを見る
                     </Button>
                 )}
