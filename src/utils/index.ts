f8e8e4423470429e5a934b59d3a01e0ecc12f769
Haruma HIRABAYASHI <hrm1810884@g.ecc.u-tokyo.ac.jp>
2024-07-04 22:49:31 +0900

    add stage switcher utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,4 +3,5 @@ export { delay } from "./delay";
 export { getBaseUrl } from "./getBaseUrl";
 export { getSocketUrl } from "./getSocketUrl";
 export { guardRecursiveUndef, guardUndef } from "./guardUndef";
+export { stageSwitcher } from "./stageSwitcher";
 export * from "./usecaseResult";
